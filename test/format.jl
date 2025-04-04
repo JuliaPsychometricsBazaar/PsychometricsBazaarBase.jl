000ec3f3c797b7461b34bc61567ac9c72ddb89cd
Frankie Robertson <frankie@robertson.name>
2025-04-04 13:15:37 +0300

    Remove format test

--- test/format.jl
+++
@@ -1,8 +0,0 @@
-using JuliaFormatter
-using PsychometricsBazaarBase
-
-@testset "format" begin
-    dir = pkgdir(PsychometricsBazaarBase)
-    @test format(dir * "/src"; overwrite = false)
-    @test format(dir * "/test"; overwrite = false)
-end
