80129606a808a12ebad46f0b179adcf8da17f4b1
Frankie Robertson <frankie@robertson.name>
2025-06-08 14:50:31 +0300

    Fix up the multidimensional grid integrator

--- src/integrators/fixed.jl
+++ src/integrators/fixed.jl
@@ -39,33 +39,47 @@ function (integrator::FixedGridIntegrator)(args...; kwargs...)
 end
 
 struct PreallocatedFixedGridIntegrator{ContainerT <:
-                                       Union{Vector{Float64}, Matrix{Float64}}} <:
+                                       Union{Vector{Float64}, Matrix{Float64}},
+                                       FixedGridIntegratorT <: FixedGridIntegrator} <:
        Integrator
-    inner::FixedGridIntegrator
+    inner::FixedGridIntegratorT
     buf::ContainerT
 
     function PreallocatedFixedGridIntegrator(inner::FixedGridIntegrator{Vector{Float64}})
         quadpts = length(inner.grid)
         buf = Vector{Float64}(undef, quadpts)
-        new{Vector{Float64}}(inner, buf)
+        new{Vector{Float64}, FixedGridIntegrator{Vector{Float64}}}(inner, buf)
     end
 
     function PreallocatedFixedGridIntegrator(inner::FixedGridIntegrator{Vector{Vector{Float64}}})
         quadpts = length(inner.grid)
         # XXX: In general this is wrong. The output dimension could be anything.
+        # TODO: Instead it be that we have a maximum output dimension specified at construction time
         dim = length(inner.grid[1])
         buf = Matrix{Float64}(undef, quadpts, dim)
-        new{Matrix{Float64}}(inner, buf)
+        new{Matrix{Float64}, FixedGridIntegrator{Vector{Vector{Float64}}}}(inner, buf)
     end
 end
 
-function (integrator::PreallocatedFixedGridIntegrator)(
-        f::F,
-        ncomp::Int = 0
+function (integrator::PreallocatedFixedGridIntegrator{Vector{Float64}})(
+    f::F,
+    ncomp::Int = 0
 ) where {F}
     if ncomp == 0 || ncomp == 1
         integrator.buf .= f.(integrator.inner.grid)
         BareIntegrationResult(sum(integrator.buf))
+    else
+        error("ncomp must be 0 or 1 for FixedGridIntegrator with Vector{Float64} buffer")
+    end
+end
+
+function (integrator::PreallocatedFixedGridIntegrator{Matrix{Float64}})(
+    f::F,
+    ncomp::Int = 0
+) where {F}
+    if ncomp == 0 || ncomp == 1
+        integrator.buf[:, 1] .= f.(integrator.inner.grid)
+        BareIntegrationResult(sum(@view integrator.buf[:, 1]))
     else
         buf_rows = eachrow(integrator.buf)
         buf_rows .= f.(integrator.inner.grid)
