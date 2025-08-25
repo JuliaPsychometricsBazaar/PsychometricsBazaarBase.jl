febdaf6f569237546e3e4204315730de6a428255
Frankie Robertson <frankie@robertson.name>
2025-08-25 12:53:31 +0300

    Add some show utils

--- src/PsychometricsBazaarBase.jl
+++ src/PsychometricsBazaarBase.jl
@@ -5,6 +5,18 @@ using DocStringExtensions
 public Parameters, ConfigTools, IntegralCoeffs, Integrators, ConstDistributions,
        Interpolators, Optimizers
 
+function show_into_string(obj, mime::MIME = MIME("text/plain"))
+    buf = IOBuffer()
+    show(buf, mime, rules)
+    return String(take!(buf))
+end
+
+function show_into_buf(obj, mime::MIME = MIME("text/plain"))
+    buf = IOBuffer()
+    show(buf, mime, rules)
+    return seekstart(buf)
+end
+
 include("./vendor/IndentWrappers.jl")
 include("./vendor/Parameters.jl")
 include("./ConfigTools.jl")
